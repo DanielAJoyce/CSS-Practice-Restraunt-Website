c52e8d70e64c2e8ec7895bb6e5ee774b9d660797
Daniel Joyce <Naiyon@users.noreply.github.com>
2017-07-12 22:08:55 +0100

    Fixed Navigation complete
    Fixed nav complete.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,14 @@
-$(document).ready(function(){
-	alert("Testing Testing 1,2,3");
 
+
+$(document).ready(function(){
+//Direction means to detect whether or not user is scrolling up or down.
+	$('.js-section-features').waypoint(function(direction){
+		if(direction == "down"){
+			$('nav').addClass('sticky');
+		}else{
+			$('nav').removeClass('sticky');
+		}
+	}, {
+		offset:"60px;"
+	});
 });
