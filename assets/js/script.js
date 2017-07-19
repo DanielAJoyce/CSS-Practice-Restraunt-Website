cfa8ee745bcf6ac74523cb75b9025a67a42fe4e1
Daniel Joyce <Naiyon@users.noreply.github.com>
2017-07-19 21:47:26 +0100

    Changes for navigation. Attempting to fix smooth scrolling as it's broken...
    Yup

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	//alert('hi');
+
+
+	$('.js-section-features').waypoint(function(direction){
+
+		if(direction == "down"){
+			$('nav').addClass('sticky');
+			
+		}else{
+			$('nav').removeClass('sticky');
+		}
+	}, {offset:'60px'});
+
+
+	/*Navigation scroll */
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+
+});
